624f2e3032a18c94cb729dca75e8eb69bc45e6ae
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-11 23:42:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -210,7 +210,21 @@ def player_stats(player_name)
 end
 
 def big_shoe_rebounds
+  biggest_shoe_size = []
+  data = game_hash()
+  data.each do |home_away, team_data|
+    team_data.each do |name_color_players, associated_info|
+      if name_color_players == :players
+        associated_info.each do |player_stats|
+          binding.pry
+          
+        end
+      end
+    end
+  end
 
+
+end
 
 
 
