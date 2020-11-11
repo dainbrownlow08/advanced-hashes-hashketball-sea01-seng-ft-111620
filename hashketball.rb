ea1f8d6cd90988d4fe8803f008c2dfa4ce8b05ca
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-11 23:29:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,7 @@ def player_numbers(team_name)
     if data[home_away][:team_name] == team_name
       players = data[home_away][:players]
       players.each do |player|
-        jersey_number_array << player[:shoe]
+        binding.pry
       end
     end
   end
